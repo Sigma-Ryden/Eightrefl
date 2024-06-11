8037b75a79b3363f65d9de9f60e5ed6ce650ead4
Sigma-Ryden <math.divergent@gmail.com>
2024-06-11 20:51:17 +0300

    in progress

--- test/TestLibrary.cpp
+++ test/TestLibrary.cpp
@@ -11,6 +11,8 @@ struct TestReflectableStruct
     void Foo(TestReflectableDeclaratonOnlyWithUsingStruct) {}
 };
 
+struct TestReflectableWithoutDeclarationStruct {};
+
 } // TEST_SPACE
 
 REFLECTABLE_DECLARATION(TestReflectableDeclaratonOnlyStruct)
@@ -25,6 +27,10 @@ REFLECTABLE_DECLARATION_INIT()
 REFLECTABLE(TestReflectableStruct)
     FUNCTION(Foo) // will implicitly add TestReflectableDeclaratonOnlyWithUsingStruct to registry
 REFLECTABLE_INIT()
+
+// will not compile, since rrquires REFLECTABLE_DECLARATION(...), that contains reflectable_traits
+//REFLECTABLE(TestReflectableWithoutDeclarationStruct)
+//REFLECTABLE_INIT()
 
 TEST(TestLibrary, TestReflectableRegistry)
 {
@@ -58,6 +64,7 @@ TEST(TestLibrary, TestReflectableRegistry)
         EXPECT("reflection", type->reflection != nullptr);
     }
 }
+
 
 TEST_SPACE()
 {
@@ -98,3 +105,106 @@ TEST(TestLibrary, TestDeparen)
     EXPECT("template", reflection->function.find("Template<int>") != nullptr);
     EXPECT("template_with_parentheses", reflection->function.find("Template<float, bool>") != nullptr);
 }
+
+
+TEST_SPACE()
+{
+
+struct TestParentTopBaseStruct {};
+struct TestParentBaseStruct : TestParentTopBaseStruct {};
+
+struct TestParentInterface
+{
+    virtual void Foo() = 0;
+};
+
+struct TestParentDerivedStruct : TestParentBaseStruct, TestParentInterface
+{
+    void Foo() override {}
+};
+
+} // TEST_SPACE
+
+REFLECTABLE_DECLARATION(TestParentTopBaseStruct)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestParentTopBaseStruct)
+REFLECTABLE_INIT()
+
+REFLECTABLE_DECLARATION(TestParentBaseStruct)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestParentBaseStruct)
+    PARENT(TestParentTopBaseStruct)
+REFLECTABLE_INIT()
+
+REFLECTABLE_DECLARATION(TestParentInterface)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestParentInterface)
+    FUNCTION(Foo)
+REFLECTABLE_INIT()
+
+REFLECTABLE_DECLARATION(TestParentDerivedStruct)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestParentDerivedStruct)
+    PARENT(TestParentBaseStruct)
+    PARENT(TestParentInterface)
+    FUNCTION(Foo)
+REFLECTABLE_INIT()
+
+TEST(TestLibrary, TestParent)
+{
+    auto type = rew::global.find("TestParentDerivedStruct");
+
+    ASSERT("type", type != nullptr);
+
+    auto reflection = type->reflection;
+
+    ASSERT("reflection", reflection != nullptr);
+
+    auto base = reflection->parent.find("TestParentBaseStruct");
+
+    ASSERT("base", base != nullptr);
+    EXPECT("base-type", base->type == rew::global.find(base->name));
+
+    auto base_base = base->type->reflection->parent.find("TestParentTopBaseStruct");
+
+    ASSERT("base-base", base_base != nullptr);
+    EXPECT("base-base-type", base_base->type == rew::global.find(base_base->name));
+    EXPECT("non_directed_base", reflection->parent.find(base_base->name) == nullptr);
+
+    auto interface = reflection->parent.find("TestParentInterface");
+
+    ASSERT("interface", interface != nullptr);
+    EXPECT("interface-type", interface->type == rew::global.find(interface->name));
+}
+
+
+TEST_SPACE()
+{
+
+struct TestMetaStruct {};
+
+} // TEST_SPACE
+
+REFLECTABLE_DECLARATION(TestMetaStruct)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestMetaStruct)
+REFLECTABLE_INIT()
+
+TEST(TestLibrary, TestMeta)
+{
+    auto type = rew::global.find("TestMetaStruct");
+
+    ASSERT("type", type != nullptr);
+
+    auto reflection = type->reflection;
+
+    ASSERT("reflection", reflection != nullptr);
+}
+
+// TODO: add test custom reflectable registry
+// TODO: add test reflectable registry reallocation
