a300f145cea7fdc3e8cc1cff15b8319e84491032
Sigma-Ryden <math.divergent@gmail.com>
2024-06-17 21:30:22 +0300

    in progress

--- include/Rew/BuiltIn/iostream.hpp
+++ include/Rew/BuiltIn/iostream.hpp
@@ -3,6 +3,8 @@
 
 #include <Rew/Reflectable.hpp>
 
+#include <Rew/BuiltIn/locale.hpp>
+
 #include <ios> // ios_base
 
 REFLECTABLE_USING(std_ios_base_openmode, std::ios_base::openmode)
@@ -47,6 +49,13 @@ REFLECTABLE_DECLARATION(std_ios_base_event_callback)
 REFLECTABLE_DECLARATION_INIT()
 
 
+REFLECTABLE_USING(std_ios_base_event_failure, std::ios_base::failure)
+
+REFLECTABLE_DECLARATION(std_ios_base_event_failure)
+    BUILTIN_REFLECTABLE()
+REFLECTABLE_DECLARATION_INIT()
+
+
 REFLECTABLE_DECLARATION(std::ios_base)
     BUILTIN_REFLECTABLE()
 REFLECTABLE_DECLARATION_INIT()
@@ -61,6 +70,44 @@ REFLECTABLE(std::ios_base)
     FUNCTION(precision, typename R::streamsize(typename R::streamsize))
     FUNCTION(width, typename R::streamsize() const)
     FUNCTION(width, typename R::streamsize(typename R::streamsize))
+    FUNCTION(imbue)
+    FUNCTION(getloc)
+    FUNCTION(xalloc)
+    FUNCTION(iword)
+    FUNCTION(pword)
+    FUNCTION(register_callback, void(std_ios_base_event_callback, int))
+    FUNCTION(sync_with_stdio, bool(bool))
+    PROPERTY(app, std_ios_base_openmode)
+    PROPERTY(binary, std_ios_base_openmode)
+    PROPERTY(in, std_ios_base_openmode)
+    PROPERTY(out, std_ios_base_openmode)
+    PROPERTY(trunc, std_ios_base_openmode)
+    PROPERTY(ate, std_ios_base_openmode)
+    PROPERTY(dec, std_ios_base_fmtflags)
+    PROPERTY(oct, std_ios_base_fmtflags)
+    PROPERTY(hex, std_ios_base_fmtflags)
+    PROPERTY(basefield, std_ios_base_fmtflags)
+    PROPERTY(left, std_ios_base_fmtflags)
+    PROPERTY(right, std_ios_base_fmtflags)
+    PROPERTY(internal, std_ios_base_fmtflags)
+    PROPERTY(adjustfield, std_ios_base_fmtflags)
+    PROPERTY(scientific, std_ios_base_fmtflags)
+    PROPERTY(fixed, std_ios_base_fmtflags)
+    PROPERTY(floatfield, std_ios_base_fmtflags)
+    PROPERTY(boolalpha, std_ios_base_fmtflags)
+    PROPERTY(showbase, std_ios_base_fmtflags)
+    PROPERTY(showpoint, std_ios_base_fmtflags)
+    PROPERTY(showpos, std_ios_base_fmtflags)
+    PROPERTY(skipws, std_ios_base_fmtflags)
+    PROPERTY(unitbuf, std_ios_base_fmtflags)
+    PROPERTY(uppercase, std_ios_base_fmtflags)
+    PROPERTY(goodbit, std_ios_base_iosstate)
+    PROPERTY(badbit, std_ios_base_iosstate)
+    PROPERTY(failbit, std_ios_base_iosstate)
+    PROPERTY(eofbit, std_ios_base_iosstate)
+    PROPERTY(beg, std_ios_base_seekdir)
+    PROPERTY(end, std_ios_base_seekdir)
+    PROPERTY(cur, std_ios_base_seekdir)
 REFLECTABLE_INIT()
 
 #endif // REW_BUILTIN_IOSTREAM_HPP
