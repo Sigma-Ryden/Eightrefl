93a3dffa3381260c3eb7392d69d72eb8e468a35d
Sigma-Ryden <math.divergent@gmail.com>
2024-08-05 19:58:32 +0300

    in progress

--- test/TestEnum.cpp
+++ test/TestEnum.cpp
@@ -3,23 +3,23 @@
 TEST_SPACE()
 {
 
-enum TestEnum { Blue, Green, Red, Alpha };
+enum TestEnumType { Blue, Green, Red, Alpha };
 
 } // TEST_SPACE
 
-REFLECTABLE_DECLARATION(TestEnum)
+REFLECTABLE_DECLARATION(TestEnumType)
 REFLECTABLE_DECLARATION_INIT()
 
-REFLECTABLE(TestEnum)
-    META("Blue", TestEnum::Blue)
-    META("Green", TestEnum::Green)
-//  META("Red", TestEnum::Red) // skip for test
-    META("Alpha", Alpha) // since TestEnum is no scoped enum, we can ommit 'TestEnum::'
+REFLECTABLE(TestEnumType)
+    META("Blue", TestEnumType::Blue)
+    META("Green", TestEnumType::Green)
+//  META("Red", TestEnumType::Red) // skip for test
+    META("Alpha", Alpha) // since TestEnumType is no scoped enum, we can ommit 'TestEnum::'
 REFLECTABLE_INIT()
 
 TEST(TestCommon, TestEnum)
 {
-    auto type = rew::global.find("TestEnum");
+    auto type = rew::global.find("TestEnumType");
 
     ASSERT("type", type != nullptr);
 
@@ -36,22 +36,21 @@ TEST(TestCommon, TestEnum)
 TEST_SPACE()
 {
 
-enum TestScopedEnum { Up, Down, Size };
+enum class TestScopedEnumType { Up, Down, Size };
 
 } // TEST_SPACE
 
-REFLECTABLE_DECLARATION(TestScopedEnum)
+REFLECTABLE_DECLARATION(TestScopedEnumType)
 REFLECTABLE_DECLARATION_INIT()
 
-REFLECTABLE(TestScopedEnum)
-    META("Up", TestScopedEnum::Up)
-    META("Down", TestScopedEnum::Down)
-    META("Size", (int)TestScopedEnum::Size)
+REFLECTABLE(TestScopedEnumType)
+    META("Up", TestScopedEnumType::Up)
+    META("Down", TestScopedEnumType::Down)
 REFLECTABLE_INIT()
 
 TEST(TestCommon, TestScopedEnum)
 {
-    auto type = rew::global.find("TestScopedEnum");
+    auto type = rew::global.find("TestScopedEnumType");
 
     ASSERT("type", type != nullptr);
 
@@ -61,5 +60,71 @@ TEST(TestCommon, TestScopedEnum)
 
     EXPECT("meta-1", reflection->meta.find("Up") != nullptr);
     EXPECT("meta-2", reflection->meta.find("Down") != nullptr);
-    EXPECT("meta-3", reflection->meta.find("Size") != nullptr);
+}
+
+
+struct TestEnumAsStringInjection : rew::injectable_t
+{
+    rew::type_t* xxtype = nullptr;
+    std::vector<std::string> xxenum_as_string;
+
+    TestEnumAsStringInjection(rew::type_t* type) : xxtype(type)
+    {
+    }
+    
+    ~TestEnumAsStringInjection()
+    {
+        rew::find_or_add_meta(xxtype->reflection, "EnumAsString", xxenum_as_string);
+    }
+
+    template <typename ReflectableType>
+    void type(rew::type_t& type)
+    {
+        xxenum_as_string.resize((std::size_t)ReflectableType::Size);
+    }
+
+    template <typename ReflectableType, typename MetaType>
+    void meta(std::string const& name, std::any& meta)
+    {
+        auto enumerator = (std::size_t)std::any_cast<MetaType&>(meta);
+        if (enumerator < (std::size_t)ReflectableType::Size)
+        {
+            xxenum_as_string[enumerator] = name;
+        }
+    }
+};
+
+REFLECTABLE_DECLARATION(TestEnumAsStringInjection)
+REFLECTABLE_DECLARATION_INIT()
+
+REFLECTABLE(TestEnumAsStringInjection)
+REFLECTABLE_INIT()
+
+TEST(TestCommon, TestEnumAsString)
+{
+    auto type = rew::global.find("TestScopedEnumType");
+
+    ASSERT("type", type != nullptr);
+
+    auto reflection = type->reflection;
+
+    ASSERT("reflection", reflection != nullptr);
+
+    rew::find_or_add_injection<TestScopedEnumType, TestEnumAsStringInjection>(type);
+
+    auto injection = type->injection.find("TestEnumAsStringInjection");
+
+    ASSERT("injection", injection != nullptr);
+
+    {
+        auto enum_as_string = TestEnumAsStringInjection(type);
+        injection->call(enum_as_string);
+    }
+
+    auto& enum_as_string = std::any_cast<std::vector<std::string>&>
+    (
+        *type->reflection->meta.find("EnumAsString")
+    );
+
+    EXPECT("enum_as_string", enum_as_string[(std::size_t)TestScopedEnumType::Down] == "Down");
 }
