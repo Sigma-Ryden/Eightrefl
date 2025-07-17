a5f81ddf865b350c1d7e5a1f1ae7fc3212b5f04d
Sigma-Ryden <math.divergent@gmail.com>
2025-07-17 21:50:55 +0300

    Up

--- test/TestBitset.cpp
+++ test/TestBitset.cpp
@@ -1,76 +1,76 @@
-// #include <EightreflTestingBase.hpp>
+#include <EightreflTestingBase.hpp>
 
-// #include <Eightrefl/BuiltIn/bitset.hpp>
+#include <Eightrefl/BuiltIn/bitset.hpp>
 
-// TEST(TestBuiltin, TestBitset)
-// {
-//     eightrefl::reflectable<std::bitset<128>>();
+TEST(TestBuiltin, TestBitset)
+{
+    eightrefl::reflectable<std::bitset<128>>();
 
-//     auto type = eightrefl::global.find("std::bitset<128>");
+    auto type = eightrefl::global.find("std::bitset<128>");
 
-//     ASSERT("type", type != nullptr);
-//     EXPECT("type-name", type->name == "std::bitset<128>");
-//     EXPECT("type-size", type->size == sizeof(std::bitset<128>));
-//     EXPECT("type-context", type->context != nullptr);
+    ASSERT("type", type != nullptr);
+    EXPECT("type-name", type->name == "std::bitset<128>");
+    EXPECT("type-size", type->size == sizeof(std::bitset<128>));
+    EXPECT("type-context", type->context != nullptr);
 
-//     auto reflection = type->reflection;
+    auto reflection = type->reflection;
 
-//     ASSERT("reflection", reflection != nullptr);
-//     EXPECT("reflection-name", reflection->name == "std::bitset<128>");
+    ASSERT("reflection", reflection != nullptr);
+    EXPECT("reflection-name", reflection->name == "std::bitset<128>");
 
-//     EXPECT("factory-R()", reflection->factory.find("std::bitset<128>()") != nullptr);
-//     EXPECT("factory-R(R const&)", reflection->factory.find("std::bitset<128>(std::bitset<128> const&)") != nullptr);
+    EXPECT("factory-R()", reflection->factory.find("std::bitset<128>()") != nullptr);
+    EXPECT("factory-R(R const&)", reflection->factory.find("std::bitset<128>(std::bitset<128> const&)") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-//     EXPECT("factory-R(unsigned long)", reflection->factory.find("std::bitset<128>(unsigned long)") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("factory-R(unsigned long)", reflection->factory.find("std::bitset<128>(unsigned long)") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     EXPECT("factory-R(unsigned long long)", reflection->factory.find("std::bitset<128>(unsigned long long)") != nullptr);
+    EXPECT("factory-R(unsigned long long)", reflection->factory.find("std::bitset<128>(unsigned long long)") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-//     EXPECT("factory-R(std::string const&, std::size_t)", reflection->factory.find("std::bitset<128>(std::string const&, std::size_t)") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("factory-R(std::string const&, std::size_t)", reflection->factory.find("std::bitset<128>(std::string const&, std::size_t)") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     EXPECT("factory-R(std::string const&)", reflection->factory.find("std::bitset<128>(std::string const&)") != nullptr);
+    EXPECT("factory-R(std::string const&)", reflection->factory.find("std::bitset<128>(std::string const&)") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-//     EXPECT("factory-R(char const*, std::size_t)", reflection->factory.find("std::bitset<128>(char const*, std::size_t)") != nullptr);
-//     EXPECT("factory-R(char const*)", reflection->factory.find("std::bitset<128>(char const*)") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("factory-R(char const*, std::size_t)", reflection->factory.find("std::bitset<128>(char const*, std::size_t)") != nullptr);
+    EXPECT("factory-R(char const*)", reflection->factory.find("std::bitset<128>(char const*)") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     ASSERT("function-operator=", reflection->function.find("operator=") != nullptr);
-//     ASSERT("function-operator==", reflection->function.find("operator==") != nullptr);
+    ASSERT("function-operator=", reflection->function.find("operator=") != nullptr);
+    ASSERT("function-operator==", reflection->function.find("operator==") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-// //  ASSERT("function-operator[]", reflection->function.find("operator[]") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+//  ASSERT("function-operator[]", reflection->function.find("operator[]") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     EXPECT("function-test", reflection->function.find("test") != nullptr);
+    EXPECT("function-test", reflection->function.find("test") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-//     EXPECT("function-all", reflection->function.find("all") != nullptr);
-//     EXPECT("function-any", reflection->function.find("any") != nullptr);
-//     EXPECT("function-none", reflection->function.find("none") != nullptr);
-//     EXPECT("function-count", reflection->function.find("count") != nullptr);
-//     EXPECT("function-size", reflection->function.find("size") != nullptr);
-//     EXPECT("function-operator&=", reflection->function.find("operator&=") != nullptr);
-//     EXPECT("function-operator|=", reflection->function.find("operator|=") != nullptr);
-//     EXPECT("function-operator^=", reflection->function.find("operator^=") != nullptr);
-//     EXPECT("function-operator~", reflection->function.find("operator~") != nullptr);
-//     EXPECT("function-operator<<", reflection->function.find("operator<<") != nullptr);
-//     EXPECT("function-operator<<=", reflection->function.find("operator<<=") != nullptr);
-//     EXPECT("function-operator>>", reflection->function.find("operator>>") != nullptr);
-//     EXPECT("function-operator>>=", reflection->function.find("operator>>=") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("function-all", reflection->function.find("all") != nullptr);
+    EXPECT("function-any", reflection->function.find("any") != nullptr);
+    EXPECT("function-none", reflection->function.find("none") != nullptr);
+    EXPECT("function-count", reflection->function.find("count") != nullptr);
+    EXPECT("function-size", reflection->function.find("size") != nullptr);
+    EXPECT("function-operator&=", reflection->function.find("operator&=") != nullptr);
+    EXPECT("function-operator|=", reflection->function.find("operator|=") != nullptr);
+    EXPECT("function-operator^=", reflection->function.find("operator^=") != nullptr);
+    EXPECT("function-operator~", reflection->function.find("operator~") != nullptr);
+    EXPECT("function-operator<<", reflection->function.find("operator<<") != nullptr);
+    EXPECT("function-operator<<=", reflection->function.find("operator<<=") != nullptr);
+    EXPECT("function-operator>>", reflection->function.find("operator>>") != nullptr);
+    EXPECT("function-operator>>=", reflection->function.find("operator>>=") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     EXPECT("function-set", reflection->function.find("set") != nullptr);
-//     EXPECT("function-reset", reflection->function.find("reset") != nullptr);
-//     EXPECT("function-flip", reflection->function.find("flip") != nullptr);
-//     EXPECT("function-to_string", reflection->function.find("to_string") != nullptr);
+    EXPECT("function-set", reflection->function.find("set") != nullptr);
+    EXPECT("function-reset", reflection->function.find("reset") != nullptr);
+    EXPECT("function-flip", reflection->function.find("flip") != nullptr);
+    EXPECT("function-to_string", reflection->function.find("to_string") != nullptr);
 
-//     #ifdef EIGHTREFL_FULLY_ENABLE
-//     EXPECT("function-to_ulong", reflection->function.find("to_ulong") != nullptr);
-//     #endif // EIGHTREFL_FULLY_ENABLE
+    #ifdef EIGHTREFL_FULLY_ENABLE
+    EXPECT("function-to_ulong", reflection->function.find("to_ulong") != nullptr);
+    #endif // EIGHTREFL_FULLY_ENABLE
 
-//     EXPECT("function-to_ullong", reflection->function.find("to_ullong") != nullptr);
-// }
+    EXPECT("function-to_ullong", reflection->function.find("to_ullong") != nullptr);
+}
